1cf37bba2be0f6c01d2cf80cebe63578450a22fb
lucas55i <lucas_55i@outlook.com>
2020-07-23 21:09:44 -0300

    Estudo dia 23/07/2020

---
+++ Condições/condicoes001.js
@@ -0,0 +1,7 @@
+var vel =  10
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //Condição simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!!')
+}
+
+console.log('Dirija sempre com cinto de segurança')
